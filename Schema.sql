2e455e09a21d92307ed4d615bad6aa52a3b5f89f
Richard.Sadowski <Richard.Sadowski@target.com>
2022-02-08 06:42:53 -0600

    Add, Commit, Push

--- Schema.sql
+++ Schema.sql
@@ -48,4 +48,16 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
-SELECT * FROM departments;
+SELECT * FROM employees;
+DROP TABLE titles CASCADE
+CREATE TABLE titles (
+	emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
+SELECT * FROM titles;
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
